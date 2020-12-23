c4b46a47a0de5921f642ce7787ec5fbdb8ea52f1
ItsmeLee <arsmagica90@gmail.com>
2020-12-23 12:38:46 +0900

    Send data
    Learned how to send some datas from the parent component to child component.

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Potato from "./Potato";
 
+function Food({ fav }){
+  console.log(fav);
+  return <h1>I love {fav}</h1>
+}
 // Component
 function App() {
   return (
     <div className="App">
-      <Potato />
+      <Food fav="kimchi"/>
+      <Food fav="Ramen" />
+      <Food fav="chukumi"/>
     </div>
     
   );
