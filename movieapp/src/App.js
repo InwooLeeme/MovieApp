47ebe57c5c4ba901737d1b724a23e4d78769a543
ItsmeLee <arsmagica90@gmail.com>
2020-12-24 12:03:45 +0900

    Get API Url
    get json datas from movies api url.
    Add the movies array in state object.

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,30 +1,21 @@
 import React from "react";
-//import PropTypes from "prop-types";
+import Axios from "axios";
 
 class App extends React.Component{
   state = {
-    // Object
-    count: 0
+    isLoading : true,
+    movies : [],
   };
-  add = () => {
-    this.setState(current => ({count : current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({ count : current.count - 1}));
-  };
-  componentDidMount(){
-    console.log(`component Rendered`);
+
+  getMovies = async () => {
+    const movies = await Axios.get("https://yts-proxy.now.sh/list_movies.json");
   }
-  componentDidUpdate(){
-    console.log('i just updated');
+  componentDidMount(){
+    this.getMovies();
   }
   render(){
-    console.log('Im rendering');
-    return <div>
-      <h1>The number is {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
   }
 }
 
