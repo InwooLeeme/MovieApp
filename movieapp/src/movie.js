da4172795840a7b94026dec491d719f1fe0d91ca
ItsmeLee <arsmagica90@gmail.com>
2020-12-24 18:12:35 +0900

    Change summary part
    Changed the character of the sumary part to display up to 180 characters.

--- movieapp/src/movie.js
+++ movieapp/src/movie.js
@@ -9,7 +9,7 @@ function Movie({ title, summary, poster, year, genres}){
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
             <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
-            <p className="movie__summary">{summary}</p>
+            <p className="movie__summary">{summary.slice(0,180)}...</p>
         </div>
     </div>)
 }
