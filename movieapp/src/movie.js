bd30fab7d491f10d8036c7642042ccfc9552f3e8
ItsmeLee <arsmagica90@gmail.com>
2020-12-24 14:21:17 +0900

    Change class tag for fix the error
    Changed class to className for fix the console error.

--- movieapp/src/movie.js
+++ movieapp/src/movie.js
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 
 function Movie({ title, summary, poster, year, genres}){
     return (
-    <div class="movie">
+    <div className="movie">
         <img src={poster} alt={title} title={title}></img>
-        <div class="movie__data">
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie__summary">{summary}</p>
-            <span class="movie__genres">{genres}</span>
+        <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
+            <p className="movie__summary">{summary}</p>
         </div>
     </div>)
 }
@@ -20,7 +20,7 @@ Movie.propTypes = {
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
     poster : PropTypes.string.isRequired,
-    genres : PropTypes.array.isRequired
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie
