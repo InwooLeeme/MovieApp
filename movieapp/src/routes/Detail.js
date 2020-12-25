0cac6ced36ce110f7c08c9a96ba1fb58b8d3a397
ItsmeLee <arsmagica90@gmail.com>
2020-12-26 04:16:06 +0900

    Add movie detail function
    Added movie detail function that when user click the movie card show the movie detail.

--- movieapp/src/routes/Detail.js
+++ movieapp/src/routes/Detail.js
@@ -1,8 +1,37 @@
 import React from "react";
 
-function Detail(props){
-    console.log(props);
-    return (<span>Hello</span>)
+class Detail extends React.Component{
+    componentDidMount(){
+        const {location, history } = this.props;
+        if(location.state === undefined){
+            // redirect
+            history.push("/");
+        }
+    }
+    render(){
+        const {location:{state}} = this.props;
+        const {year, title, summary, genres,poster} = this.props.location.state;
+        if(state){
+            return (
+                <div className="detail">
+                    <img src={poster} alt={title} title={title}></img>
+                        <div className="detail">
+                        <h3 className="detail__title">{title}</h3>
+                        <h5 className="detail__year">{year}</h5>
+                        <ul className="detail__genres">{
+                            genres.map(
+                                (genre, index) => <li key={index} className="genres__genre">{genre}</li>
+                            )
+                        }</ul>
+                        <p className="detail__summary">{summary}</p>
+                    </div>
+                </div>
+            );
+        }
+        else{
+            return null;
+        }
+    }
 }
 
 export default Detail
