1a11bf64c0b5a6743625922d9a738b12549eead3
ItsmeLee <arsmagica90@gmail.com>
2020-12-26 02:33:08 +0900

    Testing get props
    Testing get props in movie detail page.

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App(){
@@ -10,6 +11,7 @@ function App(){
       <Navigation />
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" component={About}></Route>
+      <Route path="/movie__detail" component={Detail}></Route>
     </HashRouter>
   )
 }
