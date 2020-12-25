ad4063a0e32e412aecf676a0fc4ed244d366f7a4
ItsmeLee <arsmagica90@gmail.com>
2020-12-26 03:40:13 +0900

    Complete styling in menu
    Complete styling in menu bar.
    It looks like simple.

--- movieapp/src/components/Navigation.js
+++ movieapp/src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Navigation(){
     return (
-        <div>
+        <div className="menu">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </div>
